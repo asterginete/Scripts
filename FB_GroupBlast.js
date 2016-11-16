718a1027acd00b4f7b28842c9d04e7dca2d47d8a
asterginete <asterginete@gmail.com>
2016-11-16 22:52:21 +0900

    Facebook Group Blast Script
    This script posts advertisements to different Facebook groups with
    delay. Uses iMacros Firefox extension.

---
+++ FB_GroupBlast.js
@@ -0,0 +1,198 @@
+﻿
+var content = new Array();
+var keyword = new Array();
+var thumb = new Array();
+
+content[0] = "LOOKING<SP>FOR:<SP>Vacant<SP>Lots<SP>in<SP>Macapagal<SP>Area<SP>near<SP>MOA<BR>Preferably<SP>with<SP>a<SP>minimum<SP>lot<SP>cuts<SP>of<SP>1,000<SP>sqm<SP>and<SP>a<SP>maximum<SP>of<SP>30,000<SP>sqm.<SP><BR>Budget<SP>ranging<SP>from<SP>60k-80K<SP>per<SP>sqm.<BR><BR>Call<SP>me<SP>asap<SP>if<SP>you<SP>have<SP>any<SP>of<SP>this<SP>listings<SP>at<SP>(02)<SP>502-8786<SP>or<SP>text<SP>me<SP>0917-5203991.<BR><BR>Property<SP>Business<SP>Search<SP>Realty<SP>Corporation<BR>http://PBS.SubmitAndMatch.com";
+keyword[0] = "LOOKING FOR: Vacant Lots in Macapagal Area near MOA";
+thumb[0] = 0;
+
+content[1] = "LOOKING<SP>FOR:<SP>Old<SP>Buildings<SP>that<SP>can<SP>be<SP>renovated<SP>and<SP>be<SP>leased<SP>out<SP>or<SP>be<SP>redeveloped.<SP><BR>Preferably<SP>in<SP>the<SP>areas<SP>of<SP>Makati,<SP>Binondo,<SP>and<SP>Malate.<SP><BR>Budget<SP>ranging<SP>on<SP>the<SP>valuation<SP>of<SP>the<SP>area.<SP><SP><BR><BR>Call<SP>me<SP>asap<SP>if<SP>you<SP>have<SP>any<SP>of<SP>this<SP>listings<SP>at<SP>(02)<SP>502-8786<SP>or<SP>text<SP>me<SP>0917-5203991.<BR><BR>Property<SP>Business<SP>Search<SP>Realty<SP>Corporation<BR>http://PBS.SubmitAndMatch.com";
+keyword[1] = "LOOKING FOR: Old Buildings that can be renovated and be leased out or be redeveloped.";
+thumb[1] = 0;
+
+content[2] = "LOOKING<SP>FOR:<SP>Buildings<SP>with<SP>existing<SP>lease<SP>contracts<SP>in<SP>Makati,<SP>Binondo,<SP>and<SP>Malate.<SP><BR><BR>Call<SP>me<SP>asap<SP>if<SP>you<SP>have<SP>any<SP>of<SP>this<SP>listings<SP>at<SP>(02)<SP>502-8786<SP>or<SP>text<SP>me<SP>0917-5203991.<BR><BR>Property<SP>Business<SP>Search<SP>Realty<SP>Corporation<BR>http://PBS.SubmitAndMatch.com";
+keyword[2] = "LOOKING FOR: Buildings with existing lease contracts in Makati, Binondo, and Malate.";
+thumb[2] = 0;
+
+content[3] = "LOOKING<SP>FOR:<SP>Small<SP>office<SP>Building<SP>in<SP>Jupiter<SP>Area<SP>Makati<SP>or<SP>near<SP>that<SP>budget.<SP><BR>With<SP>a<SP>budget<SP>range<SP>of<SP>20M<SP>-30M.<BR><BR>Call<SP>me<SP>asap<SP>if<SP>you<SP>have<SP>any<SP>of<SP>this<SP>listings<SP>at<SP>(02)<SP>502-8786<SP>or<SP>text<SP>me<SP>0917-5203991.<BR><BR>Property<SP>Business<SP>Search<SP>Realty<SP>Corporation<BR>http://PBS.SubmitAndMatch.com";
+keyword[3] = "LOOKING FOR: Small office Building in Jupiter Area Makati or near that budget.";
+thumb[3] = 0;
+
+content[4] = "LOOKING<SP>FOR:<SP>Vacant<SP>Lots<SP>that<SP>can<SP>be<SP>developed<SP>for<SP>a<SP>duplex<SP>townhouse<SP>or<SP>house<SP>in<SP>the<SP>following<SP>areas:<BR>-<SP>San<SP>Antonio<SP>Village<BR>-<SP>Bel<SP>Air<BR>-<SP>Forbes<BR>-<SP>Urdaneta<SP>Village<BR>-<SP>Alabang<SP>Hills<BR>-<SP>Ayala<SP>Alabang<BR>-<SP>Tahanan<SP>Village<BR>-<SP>White<SP>Plains<BR>-<SP>Merville<BR>-<SP>or<SP>any<SP>prestige<SP>subdivisions<SP>in<SP>the<SP>Metro.<SP>Budget<SP>ranging<SP>on<SP>the<SP>valuation<SP>of<SP>the<SP>area.<SP><BR><BR>Call<SP>me<SP>asap<SP>if<SP>you<SP>have<SP>any<SP>of<SP>this<SP>listings<SP>at<SP>(02)<SP>502-8786<SP>or<SP>text<SP>me<SP>0917-5203991.<BR><BR>Property<SP>Business<SP>Search<SP>Realty<SP>Corporation<BR>http://PBS.SubmitAndMatch.com";
+keyword[4] = "LOOKING FOR: Vacant Lots that can be developed for a duplex townhouse or house in the following areas:";
+thumb[4] = 0;
+
+var iCtr = 0;
+var maxCtr = 4;
+
+postGroup("http://www.facebook.com/groups/pinoymarketplace/");
+postGroup("http://www.facebook.com/groups/pinoywalangiwanan/");
+postGroup("http://www.facebook.com/groups/318034378283951/");
+postGroup("http://www.facebook.com/groups/pinoysbm/");
+postGroup("http://www.facebook.com/groups/447725315267654/");
+postGroup("http://www.facebook.com/groups/328099770555159/");
+postGroup("http://www.facebook.com/groups/pinoypridenetwork/");
+postGroup("http://www.facebook.com/groups/217689768334062/");
+postGroup("http://www.facebook.com/groups/199751052677/");
+postGroup("http://www.facebook.com/groups/327170984024939/");
+postGroup("http://www.facebook.com/groups/104327856387824/");
+postGroup("http://www.facebook.com/groups/232566466828412/");
+postGroup("http://www.facebook.com/groups/361020183937015/");
+postGroup("http://www.facebook.com/groups/MSMOP/");
+postGroup("http://www.facebook.com/groups/363878880291166/");
+postGroup("http://www.facebook.com/groups/ajfernando27/");
+postGroup("http://www.facebook.com/groups/pamilyangofwsthemepark/");
+postGroup("http://www.facebook.com/groups/192946520758096/");
+postGroup("http://www.facebook.com/groups/OFWJILLG/");
+postGroup("http://www.facebook.com/groups/110402629044082/");
+postGroup("http://www.facebook.com/groups/manilafashiondepot/");
+postGroup("http://www.facebook.com/groups/networkbiz/");
+postGroup("http://www.facebook.com/groups/ofwnews/");
+postGroup("http://www.facebook.com/groups/180570345305832/");
+postGroup("http://www.facebook.com/groups/philippinerealestate/");
+postGroup("http://www.facebook.com/groups/ofwtkkunited/");
+postGroup("http://www.facebook.com/groups/295891353841350/");
+postGroup("http://www.facebook.com/groups/144822495652471/");
+postGroup("http://www.facebook.com/groups/www.homeofpinoylovers/");
+postGroup("http://www.facebook.com/groups/230656687040566/");
+postGroup("http://www.facebook.com/groups/kwentongmgadakila/");
+postGroup("http://www.facebook.com/groups/shashazailon/");
+postGroup("http://www.facebook.com/groups/bayaningpinoyofw/");
+postGroup("http://www.facebook.com/groups/kabalikatofwworld/");
+postGroup("http://www.facebook.com/groups/336042963122765/");
+postGroup("http://www.facebook.com/groups/245898125512859/");
+postGroup("http://www.facebook.com/groups/341480749259010/");
+postGroup("http://www.facebook.com/groups/pinoybizness/");
+postGroup("http://www.facebook.com/groups/ofwtambayannight/");
+postGroup("http://www.facebook.com/groups/406487512733703/");
+postGroup("http://www.facebook.com/groups/319287044791143/");
+postGroup("http://www.facebook.com/groups/192172990862830/");
+postGroup("http://www.facebook.com/groups/173663559411243/");
+postGroup("http://www.facebook.com/groups/323409364339633/");
+postGroup("http://www.facebook.com/groups/135019376620151/");
+postGroup("http://www.facebook.com/groups/313923715366579/");
+postGroup("http://www.facebook.com/groups/50870694310/");
+postGroup("http://www.facebook.com/groups/192137094252136/");
+postGroup("http://www.facebook.com/groups/ofwjoinforce/");
+postGroup("http://www.facebook.com/groups/223316544468552/");
+postGroup("http://www.facebook.com/groups/ZamboangaCityOnLineNegosyo/");
+postGroup("http://www.facebook.com/groups/giantslayers15/");
+postGroup("http://www.facebook.com/groups/gfcpinoy/");
+postGroup("http://www.facebook.com/groups/proudlypinaypinoy/");
+postGroup("http://www.facebook.com/groups/176592535799977/");
+postGroup("http://www.facebook.com/groups/481288091900630/");
+postGroup("http://www.facebook.com/groups/319804364723176/");
+postGroup("http://www.facebook.com/groups/343440552368177/");
+postGroup("http://www.facebook.com/groups/171838842852002/");
+postGroup("http://www.facebook.com/groups/466009110079955/");
+postGroup("http://www.facebook.com/groups/164982663559538/");
+postGroup("http://www.facebook.com/groups/mycebuproperties/");
+postGroup("http://www.facebook.com/groups/119874134764677/");
+postGroup("http://www.facebook.com/groups/ads.all.u.want/");
+postGroup("http://www.facebook.com/groups/378631425562494/");
+postGroup("http://www.facebook.com/groups/555819607765643/");
+postGroup("http://www.facebook.com/groups/bestpinoy/");
+postGroup("http://www.facebook.com/groups/306445512801679/");
+postGroup("http://www.facebook.com/groups/earnmoneyswa/");
+postGroup("http://www.facebook.com/groups/289184794516972/");
+postGroup("http://www.facebook.com/groups/finance0101/");
+postGroup("http://www.facebook.com/groups/generalsantosopenforum/");
+postGroup("http://www.facebook.com/groups/281909431924742/");
+postGroup("http://www.facebook.com/groups/qeeniebraidy/");
+postGroup("http://www.facebook.com/groups/MAKILALAnatayusaBUONGMUNDO/");
+postGroup("http://www.facebook.com/groups/287758781300889/");
+postGroup("http://www.facebook.com/groups/426351850740433/");
+postGroup("http://www.facebook.com/groups/148596515237018/");
+postGroup("http://www.facebook.com/groups/pi.shopandsell/");
+postGroup("http://www.facebook.com/groups/344324472259179/");
+postGroup("http://www.facebook.com/groups/promos.discounts.freebies/");
+postGroup("http://www.facebook.com/groups/171259369561465/");
+postGroup("http://www.facebook.com/groups/Suscribers/");
+postGroup("http://www.facebook.com/groups/120754904683324/");
+postGroup("http://www.facebook.com/groups/TAGATUPI/");
+postGroup("http://www.facebook.com/groups/157377104345578/");
+postGroup("http://www.facebook.com/groups/558898450792493/");
+postGroup("http://www.facebook.com/groups/189857377780584/");
+
+function postGroup (group_link) {
+	var mDelay1 = "CODE:WAIT SECONDS=15";
+	var mDelay2 = "CODE:WAIT SECONDS=1";
+	var mDelay3 = "CODE:WAIT SECONDS=50";
+	var mGoto = "CODE:URL GOTO=" + group_link;
+	var mThumbnail = "CODE:TAG POS=1 TYPE=A ATTR=CLASS:UIThumbPagerControl_Button<SP>UIThumbPagerControl_Button_Right";
+	
+	iimPlay(mGoto);
+	
+	if (window.content.find ("Write Post")) {
+		var k = iCtr;
+		var mTextAd = "CODE:TAG POS=1 TYPE=TEXTAREA FORM=ACTION:/ajax/updatestatus.php ATTR=NAME:xhpc_message CONTENT=" + content[iCtr];
+		var j = 0;
+		for (;;) {
+			if (j == maxCtr) {
+				break;
+			}
+			if (k == maxCtr) {
+				k = 0;
+			}
+			if (window.content.find (keyword[k])) {
+			}
+			else {
+				iimPlay(mDelay2);
+				mTextAd = "CODE:TAG POS=1 TYPE=TEXTAREA FORM=ACTION:/ajax/updatestatus.php ATTR=NAME:xhpc_message CONTENT=" + content[k];
+				iimPlay(mTextAd);
+				iimPlay(mDelay1);				
+				
+				if (thumb[k] > 0) {
+					for (var i = 0; i < thumb[k]; i++) {
+						iimPlay(mThumbnail);
+						iimPlay(mDelay2);
+					}
+				}
+				
+				var postID = getPostID();
+				if (postID.length > 0) {
+					var mPost = "CODE:TAG POS=1 TYPE=INPUT:SUBMIT ATTR=ID:" + getPostID();
+					iimPlay(mPost);
+				}
+				iimPlay(mDelay3);
+				
+				iCtr++;
+				if (iCtr == maxCtr) {
+					iCtr = 0;
+				}
+				break;
+			}
+			k++;
+			j++;
+		}
+	}
+}
+
+function getPostID()
+{
+	var x = window.content.document.getElementsByTagName("input");
+	var postID = "";
+	for (var j = 0; j < x.length; j++) {
+		if ( (x[j].getAttribute("type") == "submit") && (x[j].getAttribute("value") == "Post") ) {
+			postID = x[j].id;
+		}
+	}
+	return postID;
+}
+
+function getGroups () {
+	var getLink = "CODE:TAG POS={{loop}} TYPE=A ATTR=HREF:http://www.facebook.com/groups* EXTRACT=HREF";
+	for (var i = 1; i <= 1000; i++) {
+		iimSet('loop', i);
+		iimPlay(getLink);
+		
+		strLink = iimGetLastExtract(0);
+		if (strLink == '#EANF#') {	
+			break;
+		}
+					
+		iimSet ('adlink', strLink);
+		iimPlay ('savelink');
+	}
+}
